f5c0199ce64ac11cd63309215fe7d772f33693f0
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-11 22:16:07 -0300

    tipos de dados: array eloop com each

---
+++ tipos-dados/array/index1.rb
@@ -0,0 +1,5 @@
+array = %w[um dois tres quarto]
+array1 = Array.new(5, 'oi').class
+array2 = Array.new(5, 10)
+
+puts array1
---
+++ tipos-dados/array/index2.rb
@@ -0,0 +1,2 @@
+array = [1, 2, 3 ,4]
+array.each  {  puts "O array tem o número: #{it}"}
