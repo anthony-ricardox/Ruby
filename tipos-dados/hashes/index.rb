bfdf9b87c3b7df69dbda60616fa9b55c05b435d2
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-13 21:20:42 -0300

    exercicios

--- tipos-dados/hashes/index.rb
+++ tipos-dados/hashes/index.rb
@@ -1,11 +1,30 @@
 
 
 
-hash = {:jhon => "amor", :tom => "vida"}
-hash.keys.each do |key|
-  puts "o valor correto é: "
-  puts hash[key]
-  puts "-" * 15
-end
+# hash = {:jhon => "amor", :tom => "vida"}
+# hash.keys.each do |key|
+#   puts "o valor correto é: "
+#   puts hash[key]
+#   puts "-" * 15
+# end
 
-puts hash
+# puts hash
+h1 = { a: 100, b: 200 }
+h2 = { b: 254, c: 300 }
+
+resultado = h1.merge(h2)
+
+p resultado
+
+
+primeiro_nome = "George"
+instrumento = "guitarra"
+
+membro = { primeiro_nome:, instrumento: }
+
+p membro
+
+decimal = 255
+resultado = decimal.to_s(16)
+
+puts resultado
