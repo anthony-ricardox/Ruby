528ac9518bbe2da7d230062a3f3516faad74614a
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-14 22:22:56 -0300

    utilizando  if else

---
+++ estrutura-controle/else.rb
@@ -0,0 +1,12 @@
+#analise um dia da semana
+#SE esse dia da semana  for domingo 
+#IMPRIMA que o nosso dia sera especial
+
+dia = "segunda"
+if dia == "domingo"
+  almoco = especial
+else
+  almoco = "normal"
+end
+
+puts "Hoje  nosso almoço será #{almoco}"
