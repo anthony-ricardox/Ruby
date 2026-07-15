71cd64bf8df955b6f62ce50a3756fcd5d7d8e339
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-14 23:46:49 -0300

    usando case para estrutura de cotrole, mesma coisa que switch e math

---
+++ estrutura-controle/case.rb
@@ -0,0 +1,19 @@
+#usuario vai entrar com o mês de nascimento dele
+#analisar  diversos CASOS
+#permitir que o usuario entre com esse dado
+
+puts "Digite seu mês de nascimento: "
+mes = gets.chomp.to_i
+
+case mes 
+    when 1..3
+        puts "Você nasceu no primeiro trimestre do ano"
+    when 4..6
+        puts "Você nasceu no primeiro semestre do ano"
+    when 7..9
+        puts "Você nasceu no terceiro trimestre  do ano"
+    when 10..12
+        puts "Você nasceu no segundo semestre , final do ano"
+else
+    puts "O valor do mês ìnvalido" 
+end
