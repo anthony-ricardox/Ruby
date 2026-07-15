a6695af9be7a5ea8f7ab45b83a2e1672e94fb364
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-15 03:20:08 -0300

    usando o Times , para repeticao, faz repeticao começando do 0

---
+++ estrutura-repeticao/times.rb
@@ -0,0 +1,19 @@
+
+#exemplo print
+5.times{print " oi "}
+
+#exemplo puts
+5.times{puts " oi "}
+
+#exemplo puts
+5.times do
+  puts "Oi"
+end
+
+
+#exemplo contador puts
+count = 1
+20.times do |count|
+    puts "#{count}"
+    
+end
