7a0552314c289845a0ab84ea93ba934512fa6be4
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-17 14:47:27 -0300

    descrição de como utilizar o select

--- estrutura-repeticao/select.rb
+++ estrutura-repeticao/select.rb
@@ -17,3 +17,23 @@ selecionado = hash.select do |keys, value|
 end 
 
 puts selecionado
+
+#Realiza uma seleção de elementos presentes em uma collection através de uma condição pré
+#Traz como resultado somente os valores que passam nesta condição.
+#muito útil em pesquisas
+
+#Primeiro vamos usar SELECT com ARRAY
+#Criar nosso array
+
+#(quem são os selecionados) = arrayoriginal.select do |percorre o array|
+#  (variável de percorre o array) (condição de existência) (o que ele deve executar)
+#end
+
+#puts (os selecionados)
+
+#agora vamos usar o SELECT em um HASH
+
+# (criar uma hash) = {0 => 'zero', 1 => 'um', 2 => 'dois', 3 => 'tres'}
+
+# puts 'Selecionando keys com valor maior que 0'
+# (chave escolhida) = hash.select do |key, value|
