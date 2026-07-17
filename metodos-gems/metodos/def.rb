f516e69f10021237b71408c49284039c3c17e4a7
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-17 15:18:06 -0300

    aprendendo a utilizar metedos(funções)

---
+++ metodos-gems/metodos/def.rb
@@ -0,0 +1,14 @@
+
+def oi 
+  puts "olá, tudo bem?"
+end
+ oi
+
+def nome (nome, sobrenome)
+  
+  puts "Olá, meu nome é: #{nome} #{sobrenome}"
+  
+end
+  nome = "anthony"
+  sobrenome = "ricardo"
+nome(nome, sobrenome)
