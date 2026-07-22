20ea29abbf52a274188dd4ac5ac684d0fd13952e
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-22 16:57:24 -0300

    pratica: Adicionar exemplos de construtores em poo/construtores.rb

--- poo/construtores.rb
+++ poo/construtores.rb
@@ -2,17 +2,39 @@
 #initialize. Você pode criar este método para especificar valores padrões durante a
 #construção da classe.
 
+# class Pessoa
+#   def initialize(nome, idade)
+#   @nome = nome
+#   @idade = idade
+#   end
+
+#   def conferencia
+#     puts 'Instância  de class iniciada com valores: '
+#     puts "Nome = #{@nome}"
+#     puts "Idade = #{@idade}"
+#   end
+# end
+# pessoa = Pessoa.new('Anthony', 24)
+# pessoa.conferencia
+
+
+
 class Pessoa
-  def initialize(nome, idade)
-  @nome = nome
-  @idade = idade
+  def initialize(nome, idade, sexo)
+    @nome = nome
+    @idade = idade
+    @sexo = sexo
   end
 
   def conferencia
-    puts 'Instância  de class iniciada com valores: '
-    puts "Nome = #{@nome}"
-    puts "Idade = #{@idade}"
+    puts 'Instancia de class iniciada com valores: '
+    puts "Nome: #{@nome}"
+    puts "Idade: #{@idade}"
+    puts "Sexo: #{@sexo}"
   end
 end
-pessoa = Pessoa.new('Anthony', 24)
-pessoa.conferencia
+
+  
+  pessoa = Pessoa.new('Anthony', 20, 'Masculino')
+  pessoa.conferencia
+
