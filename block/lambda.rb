7984d4db3dc1d14e4f5ed4f4e3fa765513ea2f7b
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-25 15:22:07 -0300

    duas maneira de criar um lambda

---
+++ block/lambda.rb
@@ -0,0 +1,8 @@
+#criar um lambda
+
+first_lambda = lambda {puts "meu primeiro lambda"}
+first_lambda.call
+
+#outra sintaxe de lambda
+first_lambda = -> { puts "meu primeiro lambda"}
+first_lambda.call
