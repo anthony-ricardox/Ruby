2f2f025d7654bba352f066fd5ff223bfb5e49aab
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-08-01 12:14:46 -0300

    aritmética

---
+++ tipos-de-dados/entradas-dados/numeros.rb
@@ -0,0 +1,15 @@
+
+puts "Insira um numero"
+x = gets.chomp.to_f
+puts "Insira outro numero"
+y = gets.chomp.to_f
+
+soma = x * y
+subt = x - y
+mult = x + y
+div = x / y
+
+puts soma 
+puts subt 
+puts mult 
+puts div 
