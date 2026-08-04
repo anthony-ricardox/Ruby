3295dcebd5df46c084c5b6805d55f1df36d7a647
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-08-04 17:07:11 -0300

    implementa método variádico com *args e exemplo de notas

--- orientacao-objetos/calculadora.rb
+++ orientacao-objetos/calculadora.rb
@@ -6,6 +6,20 @@ class Calculadora
   end
 end
 
+
 soma = Calculadora.new
 resultado = soma.soma(50,50, 50, 50)
 puts resultado
+
+puts '-' *10
+class Aluno
+  def notas(*args)
+    notas = []
+    notas.push(*args)
+    notas
+  end
+end
+
+a1 = Aluno.new
+notas_a1 = a1.notas(10,5.5,8.5,6.0)
+puts notas_a1
