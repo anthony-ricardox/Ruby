1c431fe01085bad4ca820a6d7af48c4daa230291
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-08-04 16:48:36 -0300

    herença/polimorfismo:herdando hereança da class animal e tendo seus proprios jeito unico de se comunicar, polimorfismo

---
+++ orientacao-objetos/animal.rb
@@ -0,0 +1,41 @@
+
+class Animal
+  attr_reader :nome
+  def initialize(nome)
+    @nome = nome
+  end
+end
+
+class Cachorro < Animal
+  attr_reader :raca
+
+  def initialize(nome, raca)
+    super(nome)
+    @raca = raca
+  end
+  def comunicar
+    puts "Au Au AUUUUU!"
+  end
+end
+
+class Gato < Animal
+  attr_reader :raca
+
+  def initialize(nome, raca)
+    super(nome)
+    @raca = raca
+  end
+  def comunicar
+    puts "Miauuuu"
+  end
+end
+
+bob = Cachorro.new('Bob', 'Pitbull')
+puts bob.nome
+puts bob.raca
+bob.comunicar
+
+nina = Gato.new('Nina', 'Sionês')
+puts nina.nome
+puts nina.raca
+nina.comunicar
