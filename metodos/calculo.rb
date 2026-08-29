f6ed27b48ec7480198c852936965a41676b86e2c
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-08-29 17:30:53 -0300

    usando calculos em metodos e paramentros

---
+++ metodos/calculo.rb
@@ -0,0 +1,25 @@
+def soma(x,y,z)
+  return x + y +z
+end
+
+puts soma(5,5,5)
+
+def subt(y,x)
+  return y-x
+end
+
+def mult(x,y)
+  return x*y
+
+end
+
+def div(y,x)
+  return y/x
+end
+
+puts subt(5,2)
+puts mult(5,2)
+puts div(5,5)
+
+
+
